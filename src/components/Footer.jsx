d5fd1f119fac27c641152b334110491249ea8481
Diego Cortes <cortesmendez.2003@gmail.com>
2024-11-28 09:16:17 -0600

    Modificacion del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-red-600 text-white py-6">
             <div className="container mx-auto text-center">
-                <p className="text-sm">Creado por <span><a href="https://cortes-dev.vercel.app/">Diego E. Cortes</a></span></p>
+                <p className="text-sm">Creado por <span className="font-bold"><a href="https://cortes-dev.vercel.app/">Diego E. Cortes</a></span></p>
             </div>
         </footer>
     );
