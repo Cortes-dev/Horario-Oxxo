c93cb0f6725ebdfa3f5daab54c8387bbba05c09b
Diego Cortes <cortesmendez.2003@gmail.com>
2024-11-28 09:14:17 -0600

    Modificacion del footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,7 @@ const Footer = () => {
     return (
         <footer className="bg-red-600 text-white py-6">
             <div className="container mx-auto text-center">
-                <p className="text-sm">&copy; 2024 Tu Empresa. Todos los derechos reservados.</p>
-                <div className="flex justify-center mt-4 space-x-6">
-                    <a href="#" className="text-gray-400 hover:text-white">Política de privacidad</a>
-                    <a href="#" className="text-gray-400 hover:text-white">Términos de uso</a>
-                    <a href="#" className="text-gray-400 hover:text-white">Contacto</a>
-                </div>
+                <p className="text-sm">Creado por <span><a href="https://cortes-dev.vercel.app/">Diego E. Cortes</a></span></p>
             </div>
         </footer>
     );
