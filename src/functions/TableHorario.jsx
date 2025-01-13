f2f020e42a5b0b7da4ff685c67d73441f09ef115
Diego Cortes <cortesmendez.2003@gmail.com>
2025-01-12 22:52:25 -0600

    Update horario

--- src/functions/TableHorario.jsx
+++ src/functions/TableHorario.jsx
@@ -1,7 +1,7 @@
 export const TableFunction = () => {
     const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
-    const horariosCompletos = ['Primera', 'Segunda', 'Tercera'];
-    const horariosButton = ['Primera', 'Segunda', 'Tercera', 'Descanso'];
+    const horariosCompletos = ['Primera', 'Segunda', 'Tercera', '12 PM - 8 PM'];
+    const horariosButton = ['Primera', 'Segunda', 'Tercera', 'Descanso', '12 PM-8 PM'];
 
 
     const usuarios = ['Diego', 'Ester', 'Marta', 'Adriana', 'Servando', 'Alejandra', 'Nancy', 'Manuel', 'Victor'];
